89e9485ae31ca73673397eae21fce5df2ea7c558
Ronan Patrick <61377954+SkrufyVI@users.noreply.github.com>
2020-04-01 15:15:57 +0100

    Update Adjacent Elements.py

--- Adjacent Elements.py
+++ Adjacent Elements.py
@@ -4,12 +4,7 @@
 # For input_array = [3,6,-2,-5,7,3], the output should be 21,
 # Because 7 and 3 produce the largest product.
 
-
-# What I want is an iterator that can loop:
-# index[0] x index[1], index[1] x [index[2], index[2] x index[3]
-# until index[-2] x index[len]
-
-# What if we create two duplicates of the same list?
+# Method 1, uses the in-builty Python sort function.
 
 
 def adjacentElementsProduct(inputArray):
@@ -29,12 +24,11 @@ def adjacentElementsProduct(inputArray):
 test_array = [3, 6, -2, -5, 7, 3]
 print(adjacentElementsProduct(test_array))
 
-
+# Method 2, More important to remember, as it doesn't abuse the sort function. 
+# Will be more useful for less friendly languages.
 
 def largest_adjacent_product(numbers):
-    '''
-    This is an important pattern to know
-    '''
+
 
     largest_product = 0
     for i in range(0, len(numbers) - 1):
@@ -44,19 +38,5 @@ def largest_adjacent_product(numbers):
 
     return largest_product
 
-'''
-[1,2,3,4,5]
 
-largest_product = 0
-i iterates until 4:
-        product = numbers[0] * numbers[0 + 1]
-        if product > largest_product:
-                largest_product = product
-
-'''
-
-# Basically, largest_product keeps getting a new figure every time there is a max multiple sum.
-# If the new
-# Largest_product keeps updating every time the multiplication gets larger.
-# No need for a cheating sort function, this is the sort function.
 
