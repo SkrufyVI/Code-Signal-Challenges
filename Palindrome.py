b654dd81a66a80a3aab8624272ad58884a5674fb
Ronan Patrick <61377954+SkrufyVI@users.noreply.github.com>
2020-04-01 15:22:37 +0100

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -3,6 +3,7 @@ Write a function that checks for a palindrome.
 Return True or False.
 '''
 
+# Method
 
 def checkPalindrome(inputString):
     reversed_inputstring = "".join(reversed(inputString))
@@ -15,3 +16,18 @@ def checkPalindrome(inputString):
     return inputString
 
 print(checkPalindrome("john"))
+
+# The above method abuses the Python inbuilt Python reverse function.
+# To reverse a string without this function, see below function:
+# Basically, create a list with the same length, and then fill it in backwards.
+
+def reverse_string(my_string):
+    a_list = list(my_string) 
+    list_length = len(a_list) 
+    b_list = [""] * list_length 
+
+    for i in range(0, list_length): 
+        b_list[i] = a_list[(list_length - i) - 1]
+       
+    return "".join(b_list) 
+
